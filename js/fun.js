8a5cde6024901e31c3221d990e27e7134a2c7636
Dave Abelson <daveabelson12@gmail.com>
2017-02-10 12:26:38 -0500

    messing with canvas

--- js/fun.js
+++ js/fun.js
@@ -4,13 +4,14 @@ $(function() {
 });
 
 function canvasSetup() {
+	
+	WIDTH = window.innerWidth;
+	HEIGHT = window.innerHeight;
 
 	var canvas = document.getElementById("dots");
-	canvas.style.width = '100%';
-	canas.style.height = '100%';
 
-	canvas.width = canvas.offsetWidth;
-	canvas.height = canvas.offsetHeight;
+	canvas.width = WIDTH;
+	canvas.height = HEIGHT;
 	var ctx = canvas.getContext("2d");
 	ctx.fillStyle="#FF0000";
 	ctx.fillRect(20,20,150,100);
