00429adbdeb9afcf891678d084fc74af5b361a8f
Dave Abelson <daveabelson12@gmail.com>
2017-02-10 11:27:50 -0500

    messing with canvas

--- js/fun.js
+++ js/fun.js
@@ -1,6 +1,10 @@
 //Fun canvas panel
-var canvas = document.getElementById("dots");
-var ctx = c.getContext("2d");
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
-ctx.stroke();
+function canvasSetup() {
+	var canvas = document.getElementById("dots");
+	var ctx = c.getContext("2d");
+	ctx.canvas.width = 400;
+	ctx.canvas.height = 200;
+	ctx.moveTo(0,0);
+	ctx.lineTo(200,100);
+	ctx.stroke();
+}
