3032d310a74bc2de4104e27546c87f6ea8c946b5
Dave Abelson <daveabelson12@gmail.com>
2017-02-10 11:42:03 -0500

    messing with canvas

--- js/fun.js
+++ js/fun.js
@@ -1,5 +1,10 @@
 //Fun canvas panel
 var canvas = document.getElementById("dots");
+canvas.style.width = '100%';
+canvas.style.height = '100%';
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+
 var ctx = c.getContext("2d");
 ctx.beginPath();
 ctx.arc(95,50,40,0,2*Math.PI);
