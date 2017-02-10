ab5c72751d62f3082d10da34bd937a06d162c44d
Dave Abelson <daveabelson12@gmail.com>
2017-02-10 11:56:02 -0500

    messing with canvas

--- js/fun.js
+++ js/fun.js
@@ -8,5 +8,8 @@ function canvasSetup() {
 	var canvas = document.getElementById("dots");
 	canvas.width = 500;
 	canvas.height = 500;
+	var ctx = canvas.getContext("2d");
+	ctx.fillStyle="#FF0000";
+	ctx.fillRect(20,20,150,100);
 }
 
